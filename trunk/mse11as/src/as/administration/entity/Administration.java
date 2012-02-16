391013bd41eea9df7447b7ecd8c8c0aebb94d0dd
ts.dimitrov8@gmail.com <ts.dimitrov8@gmail.com@b087cec2-0f1c-afe3-2f3a-fa65cbf282a6>
2012-02-16 20:33:30 +0000

    persistance xml, sample bean for table administration

---
+++ trunk/mse11as/src/as/administration/entity/Administration.java
@@ -0,0 +1,34 @@
+package as.administration.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class Administration implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	 @Id
+	 @Column(columnDefinition = "int(11)") 
+	 private long id;
+	
+	  @Column(columnDefinition = "Varchar(100)")
+	    private String titleIssue;
+
+	public String getTitleIssue() {
+		return titleIssue;
+	}
+
+	public void setTitleIssue(String titleIssue) {
+		this.titleIssue = titleIssue;
+	}
+	  
+}
