0d4e1cb381df6fb5ef051079c9939864f4e22852
ts.dimitrov8@gmail.com <ts.dimitrov8@gmail.com@b087cec2-0f1c-afe3-2f3a-fa65cbf282a6>
2012-03-24 20:58:15 +0000

    tests now are available to done by user, db functionality resolved from TestQuestion entity, crash on session missing, fetch = lazy. new entity has been created ...

--- trunk/mse11as/src/as/actions/TestSelectActions.java
+++ trunk/mse11as/src/as/actions/TestSelectActions.java
@@ -6,18 +6,21 @@ import java.util.ArrayList;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 
+import as.entities.SolvedTest;
 import as.entities.TestQuestions;
 import as.entities.Tests;
 import as.services.SelectAllTestsService;
 import as.services.TestsService;
 
 @ManagedBean(name = "testSelect")
-@RequestScoped
+@SessionScoped
 public class TestSelectActions implements Serializable {
-
+	@EJB
+	private TestsService ts;
 	private static final long serialVersionUID = 1L;
 	
 	/**
@@ -26,7 +29,7 @@ public class TestSelectActions implements Serializable {
 	@ManagedProperty(value = "#{selectAllTestsAction}")
 	public SelectAllTestsAction d;
 
-	private ArrayList<TestQuestions> selectedTestQuestions;
+	private ArrayList<TestQuestions> selectedTestQuestions = new ArrayList<TestQuestions>();
 	@SuppressWarnings("unused")
 	private ArrayList<TestQuestions> hiddenArchiveTestQuestions;
 	
@@ -38,7 +41,10 @@ public class TestSelectActions implements Serializable {
 		this.selectedTestQuestions = d.getTestQuestions();
 		this.hiddenArchiveTestQuestions = this.selectedTestQuestions;
 		for(int i=0;i<this.selectedTestQuestions.size();i++){
-			
+			for(int j=0;j<this.selectedTestQuestions.get(i).getTestAnswers().size();j++){
+			this.selectedTestQuestions.get(i).getTestAnswers().get(j).setAnswerVal(false);
+			}
+		
 		}
 	}
 
@@ -58,6 +64,25 @@ public class TestSelectActions implements Serializable {
 	public void setD(SelectAllTestsAction d) {
 		this.d = d;
 	}
+	
+	public String doneTest(ArrayList<TestQuestions> testQuestions){
+		for(int i=0;i<testQuestions.size();i++){
+			SolvedTest sTest = new SolvedTest();
+			sTest.setTestID(testQuestions.get(i).getTestID().getTestID());
+			sTest.setQuestionID(testQuestions.get(i).getQuestionID());
+			for(int j=0;j<testQuestions.get(i).getTestAnswers().size();j++){
+				boolean tempQuestionVal = testQuestions.get(i).getTestAnswers().get(j).getAnswerVal();
+				if(tempQuestionVal==true){
+					sTest.setAnswer(testQuestions.get(i).getTestAnswers().get(j).getAnswer());
+					sTest.setAnswerVal(testQuestions.get(i).getTestAnswers().get(j).getAnswerVal());
+				}
+			}
+			
+			this.ts.save(sTest);
+		}
+		return "ReadyTest";
+	}
+	
 	
 
 }
