03e527d233d03f78faff6b307e0577b2603acf92
ts.dimitrov8 <ts.dimitrov8@gmail.com>
2012-05-05 16:30:58 +0000

    Roles , Ranks and Groups - functionality created. Linked with the database; New Icons and page transformation; New page redirections; Not styled and validated;

--- mse11as/src/as/entities/Role.java
+++ mse11as/src/as/entities/Role.java
@@ -9,52 +9,18 @@ import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-/**
- * @author Stefan Stefanov
- * @dFeb 18, 2012
- */
+
 @Entity
 public class Role extends as.entities.Entity implements Serializable {
 
     @Id
     @Column(columnDefinition = "Integer", unique = true)
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer roleID;
-
-    public Integer getRoleID() {
-	return roleID;
-    }
-
-    public void setRoleID(Integer roleID) {
-	this.roleID = roleID;
-    }
-
-    public String getRoleName() {
-	return roleName;
-    }
-
-    public void setRoleName(String roleName) {
-	this.roleName = roleName;
-    }
-
-    public String getDescription() {
-	return description;
-    }
-
-    public void setDescription(String description) {
-	this.description = description;
-    }
-
-    public Set<UserProfile> getProfiles() {
-	return profiles;
-    }
-
-    public void setProfiles(Set<UserProfile> profiles) {
-	this.profiles = profiles;
-    }
 
     @Column(columnDefinition = "Varchar(50)")
     private String roleName;
@@ -62,6 +28,29 @@ public class Role extends as.entities.Entity implements Serializable {
     @Column(columnDefinition = "Varchar(250)")
     private String description;
 
-    @OneToMany(mappedBy = "roleID")
-    private Set<UserProfile> profiles;
+	public Integer getRoleID() {
+		return roleID;
+	}
+
+	public void setRoleID(Integer roleID) {
+		this.roleID = roleID;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+    
+    
 }
